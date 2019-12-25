429d7773367a5aa635ced46c4f985340adc9e24b
Michael <3yebmb@gmail.com>
2019-12-25 17:09:46 +0500

    add service, class for request permission, resource

---
+++ app/src/main/java/dev/m13d/gpslocationbackground/SendLocationToActivity.java
@@ -0,0 +1,20 @@
+package dev.m13d.gpslocationbackground;
+
+import android.location.Location;
+
+public class SendLocationToActivity {
+
+    private Location location;
+
+    public SendLocationToActivity(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
